7d3793ea2d2c53ec3b62a31f995045b2df37925b
Dror <droruri3@gmail.com>
2019-03-28 20:22:06 +0200

    Sql

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,42 @@
+package main.java.sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+
+public class Main{ //extends Application {
+
+    public static Stage getStage() {
+        return stage;
+    }
+    private static Stage stage;
+/*
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        stage = primaryStage;
+        primaryStage.show();
+    }
+*/
+
+    public static void main(String[] args) {
+        queryDB queryDB=new queryDB();
+        //launch(args);
+    /*        stage.setOnCloseRequest(event -> {
+            try {
+                queryDB.conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });*/
+        Course course=new Course("372.1.5000","kjk",3.5,3,1,"16-18",true,true,5,"jhhj","jhjh");
+        queryDB.insertCourse(course);
+        System.out.println("here");
+    }
+}
---
+++ src/main/java/sample/queryDB.java
@@ -0,0 +1,81 @@
+package main.java.sample;
+
+import java.sql.*;
+
+public class queryDB {
+
+    public static Connection conn;
+    Course course ;
+
+    private static Connection connect() {
+        // SQLite connection string
+        //  DriverManager.getConnection("jdbc:sqlite:D:\\db\\my-db.sqlite");
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String url = "jdbc:sqlite:src/main/java/database/CourseNetDB.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("success");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+    //region User
+    // return: 0 if the insert succeed else 1
+    public int insertCourse(Course course) {
+        String sql = "INSERT INTO Coureses(Name,Description,PointNum,Sylbos,Rating,CourseHours,Test,numOfWorks,attendance,courseId , Day ) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+        try (
+                Connection conn = connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1,  course.getName());
+            pstmt.setString(2,  course.getDescp());
+            pstmt.setDouble(3,  course.getPointNum());
+            pstmt.setString(4,  course.getSylabos());
+            pstmt.setDouble(5, course.getRating());
+            pstmt.setString(6, course.getHours());
+            pstmt.setBoolean(7, course.isTest());
+            pstmt.setInt(8, course.getNumOfworks());
+            pstmt.setBoolean(9, course.isAttendence());
+            pstmt.setString(10 ,course.getIdCourse());
+            pstmt.setInt(12 ,course.getDay());
+
+
+            pstmt.executeUpdate();
+            //conn.close();
+
+            return 0;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return 1;
+        }
+    }
+    private  Course SearcByValue(String parameter, String sql) throws SQLException {
+        try (Connection conn = connect();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+
+            // set the value
+            pstmt.setString(1 , parameter);
+            //
+            ResultSet rs  = pstmt.executeQuery();
+            if (rs.next())
+                // conn.close();
+                return new Course(rs.getString("courseId"),rs.getString("Name") , rs.getDouble("PointNum") , rs.getDouble("Rating") , rs.getInt("Day" ), rs.getString("CourseHours"), rs.getBoolean("Test") , rs.getBoolean("attendance") , rs.getInt("numOfWorks") , rs.getString("Description"), rs.getString("Sylbos")) ;
+            else {
+                //conn.close();
+                return null;
+            }
+            // loop through the result set
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
